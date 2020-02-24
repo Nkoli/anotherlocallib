6a03d7815b29e9a663bf932502c2de693907c0f0
ailevi <nkoliokafor@gmail.com>
2020-02-24 21:53:29 +0100

    Update base file with whitenoise and staticfile storage setting
    - Installed WhiteNoise in the middleware section
    - Added the statement to enable gzip functionality

--- anotherlocallib/settings/base.py
+++ anotherlocallib/settings/base.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'anotherlocallib.urls'
@@ -109,6 +110,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
