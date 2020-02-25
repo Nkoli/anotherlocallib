10901d32820010247441ad66e27543e3096bf9ce
ailevi <nkoliokafor@gmail.com>
2020-02-25 15:49:57 +0100

    Improve the display of the models on the admin site
    - This is done by configuring the site to use list_views and list_filters

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,8 +3,20 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
 
-admin.site.register(Author)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(Book)
-admin.site.register(BookInstance)
+# admin.site.register(Book)
+# admin.site.register(BookInstance)
 admin.site.register(Language)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
