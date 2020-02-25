36e6dfe11c474d8b4aac229474d6e4ca5f9345fc
ailevi <nkoliokafor@gmail.com>
2020-02-25 15:53:06 +0100

    Update Author's fields to display horizontally

--- catalog/admin.py
+++ catalog/admin.py
@@ -12,6 +12,7 @@ admin.site.register(Language)
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
