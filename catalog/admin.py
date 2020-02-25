c1c5fa1f2d66a582cdf8dd360759f88fd921d6b2
ailevi <nkoliokafor@gmail.com>
2020-02-25 15:57:33 +0100

    Add inline editing functionality for book instances

--- catalog/admin.py
+++ catalog/admin.py
@@ -14,9 +14,13 @@ class AuthorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
