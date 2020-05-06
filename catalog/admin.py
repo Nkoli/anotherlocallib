07f10dd5d426842c9e818be7a1dc05f687f08d8f
ailevi <nkoliokafor@gmail.com>
2020-05-06 01:21:01 +0100

    Implement flake8 styling

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
 
 # admin.site.register(Author)
 admin.site.register(Genre)
@@ -9,8 +8,10 @@ admin.site.register(Genre)
 # admin.site.register(BookInstance)
 admin.site.register(Language)
 
+
 class BooksInline(admin.TabularInline):
     model = Book
+
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
@@ -18,13 +19,16 @@ class AuthorAdmin(admin.ModelAdmin):
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
     inlines = [BooksInline]
 
+
 class BooksInstanceInline(admin.TabularInline):
     model = BookInstance
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
+
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
--- catalog/models.py
+++ catalog/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.urls import reverse # Used to generate URLs by reversing the URL patterns
-import uuid # Required for unique book instances
-from datetime import date
+from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
+import uuid  # Required for unique book instances
 
-# Create your models here.
+
 class Genre(models.Model):
     """Model representing a book genre."""
     name = models.CharField(max_length=200, help_text='Enter a book genre')
@@ -11,6 +10,7 @@ class Genre(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return self.name
+
 
 class Book(models.Model):
     """Model representing a book (but not a specific copy of a book)."""
@@ -21,7 +21,11 @@ class Book(models.Model):
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
-    isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    isbn = models.CharField(
+        'ISBN',
+        max_length=13,
+        help_text='13 Character ISBN number</a>'
+        )
 
     # ManyToManyField used because genre can contain many books. Books can cover many genres.
     # Genre class has already been defined so we can specify the object above.
@@ -31,7 +35,7 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
@@ -42,9 +46,13 @@ class Book(models.Model):
 
     display_genre.short_description = 'Genre'
 
+
 class BookInstance(models.Model):
-    """Model for representing a specific book copy (i.e. that can be borrowed from the library)""" 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular book across whole library')
+    """Model for representing a specific book copy (i.e. that can be borrowed from the library)"""
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4,
+        help_text='Unique ID for this particular book across whole library'
+        )
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
@@ -70,6 +78,7 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object"""
         return f'{self.id} ({self.book.title})'
+
 
 class Author(models.Model):
     """Model representing an author"""
