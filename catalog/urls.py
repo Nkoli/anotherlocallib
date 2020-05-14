a9a6609d6b56da6b0525413509325add2d4625f2
ailevi <nkoliokafor@gmail.com>
2020-05-15 00:43:42 +0100

    Create URLConf for newly created view

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('authors/<int:pk>/', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('allborrowedbooks/', views.LoanedBooksByLibrarianListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
 ]
