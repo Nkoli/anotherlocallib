20405b8900571694e31b1c53882ec9d3674c3ba3
ailevi <nkoliokafor@gmail.com>
2020-05-15 21:06:42 +0100

    Create Author views for adding functionality to create, edit and delete an Author's record

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,9 @@ import datetime
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.contrib.auth.decorators import permission_required
 
@@ -103,3 +104,18 @@ def renew_book_librarian(request, pk):
         'book_instance': book_instance,
     }
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = '__all__'
+
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
