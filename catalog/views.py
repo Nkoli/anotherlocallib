27ea801bf69adaea456f40bdab6ab083d6693bc9
ailevi <nkoliokafor@gmail.com>
2020-05-15 20:58:39 +0100

    Change corresponding form variable name

--- catalog/views.py
+++ catalog/views.py
@@ -90,13 +90,13 @@ def renew_book_librarian(request, pk):
         form = RenewBookForm(request.POST)
 
         if form.is_valid():
-            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.due_back = form.cleaned_data['due_back']
             book_instance.save()
             return HttpResponseRedirect(reverse('all-borrowed'))
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
         form = RenewBookForm(
-            initial={'renewal_date': proposed_renewal_date})
+            initial={'due_back': proposed_renewal_date})
         
     context = {
         'form': form,
